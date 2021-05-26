43ae3ec3e8bda2026323ea88bbcf0e7ffc7cb64b
kozinvl <vladiksystem@gmail.com>
2021-05-26 23:41:26 +0300

    Added Create Page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,12 +13,26 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params =
-      params.require(:movie).
-        permit(:title, :description, :rating, :released_on, :total_gross)
-
-    @movie.update(movie_params)
+    @movie.update(event_params)
 
     redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(event_params)
+    @movie.save
+
+    redirect_to @movie
+  end
+
+  private
+
+  def event_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
   end
 end
